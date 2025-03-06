2e25091072544ef6ea0078f2ee6024e4f2b78f65
Snigdha Tiwari <snigdha2210@gmail.com>
2025-03-06 18:09:09 -0500

    made application responsive for certain components

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,32 +1,49 @@
 // components/Sidebar.tsx
 "use client";
 
-import React from "react";
-import styles from "../app/styles/AdminPage.module.css";
+import React, { useState } from "react";
 import Image from "next/image";
+import { FaBars, FaTimes } from "react-icons/fa";
+import styles from "../app/styles/AdminPage.module.css";
 
 const Sidebar: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleSidebar = () => setOpen((prev) => !prev);
+
   return (
-    <aside className={styles.sidebar}>
-      <div className={styles.sidebarTop}>
-        <div className={styles.sidebarLogo}>
-          {" "}
-          <Image
-            src='/images/logo-white.png'
-            alt='Alma Logo'
-            width={90}
-            height={25}
-          />
+    <>
+      {/* Hamburger icon visible on small screens */}
+      <div className={styles.hamburgerIcon} onClick={toggleSidebar}>
+        <FaBars size={24} color='#000' />
+      </div>
+
+      {/* Sidebar container */}
+      <div className={`${styles.sidebar} ${open ? styles.openSidebar : ""}`}>
+        <div className={styles.sidebarHeader}>
+          <div className={styles.sidebarLogo}>
+            <Image
+              src='/images/alma-logo.png'
+              alt='Alma Logo'
+              width={100}
+              height={30}
+            />
+          </div>
+          <div className={styles.closeIcon} onClick={toggleSidebar}>
+            <FaTimes size={24} color='#fff' />
+          </div>
         </div>
         <nav className={styles.sidebarNav}>
           <a className={styles.sidebarNavItem}>Leads</a>
           <a className={styles.sidebarNavItem}>Settings</a>
         </nav>
       </div>
-      <div className={styles.sidebarBottom}>
-        <div className={styles.sidebarNavItem}>Admin</div>
-      </div>
-    </aside>
+
+      {/* Overlay when sidebar is open on small screens */}
+      {open && (
+        <div className={styles.sidebarOverlay} onClick={toggleSidebar} />
+      )}
+    </>
   );
 };
 
